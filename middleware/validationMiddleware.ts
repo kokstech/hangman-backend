ed1841c704d8ca9c7abd2e51f092d2636ad4ef5d
kokstech <stefan.ristic.koki@gmail.com>
2023-03-08 14:53:10 +0100

    status code 422

--- middleware/validationMiddleware.ts
+++ middleware/validationMiddleware.ts
@@ -7,7 +7,7 @@ const validationMiddleware = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log(errors.array()[0]);
-    return res.status(400).json({ errors: errors.array() });
+    return res.status(422).json({ errors: errors.array() });
   } else {
     next();
   }
